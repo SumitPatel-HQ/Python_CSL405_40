0a0f23dce26ee8b41d98e9507963ef0cc70367c0
Sumit Patel <136911724+SumitPatel-HQ@users.noreply.github.com>
2025-02-04 14:51:10 +0530

    Create Experiment_03.py

---
+++ Experiments/Experiment_03.py
@@ -0,0 +1,13 @@
+## Counts Number of lins , Word & Characters
+
+if __name__ == '__main__':
+    with open("D:\\Learning Hub\\Programming\\Python Programs\\College\\file.txt", "r") as file:
+
+        lines = file.readlines()
+        numberOfLines = len(lines)
+        numberOfWords = sum(len(line.split()) for line in lines)
+        numberOfCharacters = sum(len(line) for line in lines)  # Counting characters including spaces and newlines
+
+    print(f"Lines: {numberOfLines}")
+    print(f"Words: {numberOfWords}")
+    print(f"Characters: {numberOfCharacters}")

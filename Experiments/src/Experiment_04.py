5f64f5b73d3962e5e6a332228dc2cb28da60c1e9
Sumit Patel <136911724+SumitPatel-HQ@users.noreply.github.com>
2025-04-01 15:56:22 +0530

    Delete Experiments directory

--- Experiments/src/Experiment_04.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-# Get all files in the current directory
-files = [file.name for file in Path('.').iterdir() if file.is_file()]
-
-print("Files in the current directory:")
-for file in files:
-   
